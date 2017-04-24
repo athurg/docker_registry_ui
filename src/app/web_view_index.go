f59a0e3ea06d63d946e8751d481d2e15d7b71f1d
Jianbo Feng <fengjianbo@nibirutech.com>
2017-04-24 14:21:24 +0800

    修正仓库列表页面顺序不稳定的问题

--- src/app/web_view_index.go
+++ src/app/web_view_index.go
@@ -5,40 +5,40 @@ import (
 	"net/http"
 )
 
-func getRepoList() (map[string]int, error) {
+func getRepoList() ([]string, []int, error) {
 	resourceAction := ResourceActions{Type: "registry", Name: "catalog", Actions: []string{"*"}}
 	token, err := CreateToken("", CfgTokenService, []ResourceActions{resourceAction})
 	if err != nil {
-		return nil, fmt.Errorf("创建Token错误: %s", err)
+		return nil, nil, fmt.Errorf("创建Token错误: %s", err)
 	}
 	err, catalogInfo := registryClient.GetCatalog(token)
 	if err != nil {
-		return nil, fmt.Errorf("获取仓库列表错误: %s", err)
+		return nil, nil, fmt.Errorf("获取仓库列表错误: %s", err)
 	}
 
-	result := make(map[string]int)
-	for _, repo := range catalogInfo.Repositories {
+	tagCounts := make([]int, len(catalogInfo.Repositories))
+	for i, repo := range catalogInfo.Repositories {
 		resourceAction := ResourceActions{Type: "repository", Name: repo, Actions: []string{"pull"}}
 		token, err := CreateToken("", CfgTokenService, []ResourceActions{resourceAction})
 		if err != nil {
-			return nil, fmt.Errorf("创建Token错误: %s", err)
+			return nil, nil, fmt.Errorf("创建Token错误: %s", err)
 		}
 		err, info := registryClient.GetTags(repo, token)
 		if err != nil {
-			return nil, fmt.Errorf("获取仓库标签错误: %s", err)
+			return nil, nil, fmt.Errorf("获取仓库标签错误: %s", err)
 		}
 
-		result[repo] = len(info.Tags)
+		tagCounts[i] = len(info.Tags)
 	}
 
-	return result, nil
+	return catalogInfo.Repositories, tagCounts, nil
 }
 
 func ViewIndexHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	html := htmlHead
 
-	repoTagCount, err := getRepoList()
+	repos, tagCounts, err := getRepoList()
 	if err != nil {
 		html += "<p>" + err.Error() + "</p>"
 	} else {
@@ -51,8 +51,8 @@ func ViewIndexHandler(w http.ResponseWriter, r *http.Request) {
 		<div class="row">
 		<table class="table table-bordered table-hover"><thead><tr><th>仓库</th><th>镜像数量</th></tr></thead><tbody>
 		`
-		for repo, tagCount := range repoTagCount {
-			html += fmt.Sprintf("<tr><td><a href='/view/repo?name=%s'>%s</a></td><td>%d</td></tr>", repo, repo, tagCount)
+		for i, repo := range repos {
+			html += fmt.Sprintf("<tr><td><a href='/view/repo?name=%s'>%s</a></td><td>%d</td></tr>", repo, repo, tagCounts[i])
 		}
 		html += `</tbody></table></div>`
 	}
